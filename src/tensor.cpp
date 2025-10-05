f9917f0241994b0cb2f4e63eb92b10d28edb023a
siddhesh <siddheshbadnapurkar@gmail.com>
2025-10-05 19:42:17 +0000

    TODO: [update src/tensor.cpp]

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -4,17 +4,20 @@
 
 #include "../include/tensor.hpp"
 
-template <typename T, typename U>
-axon::i64 dot(const Tensor<T>& a, const Tensor<U>& b) {
-    if (a.getData().size() != b.getData().size()) {
-        throw std::range_error("The size of both Tensors must be the same");
-    }
-    axon::i64 dot = 0;
-    size_t n = a.getData().size();
+
+// ! TO REMOVE:
+// ! Causes errors with undefined references due to the templates
+// template <typename T, typename U>
+// axon::i64 dot(const Tensor<T>& a, const Tensor<U>& b) {
+//     if (a.getData().size() != b.getData().size()) {
+//         throw std::range_error("The size of both Tensors must be the same");
+//     }
+//     axon::i64 dot = 0;
+//     size_t n = a.getData().size();
     
-    for (size_t i = 0; i < n; i++) {
-        dot += int(double(a.data[i]) * double(b.data[i]));
-    }
+//     for (size_t i = 0; i < n; i++) {
+//         dot += int(double(a.data[i]) * double(b.data[i]));
+//     }
     
-    return dot;
-}
+//     return dot;
+// }
